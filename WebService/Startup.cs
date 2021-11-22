7a66c9f7f9fba787cf4813dab2525f767f80c233
Henrik Bulskov <bulskov@ruc.dk>
2021-11-22 20:25:01 +0100

    Chane to livecode 7 and add web server

--- WebService/Startup.cs
+++ WebService/Startup.cs
@@ -30,6 +30,8 @@ namespace WebService
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseFileServer();
+
             app.UseRouting();
 
             app.UseCors(x => x
