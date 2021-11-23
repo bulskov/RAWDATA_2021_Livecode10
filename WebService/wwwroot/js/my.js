de60fb4c4eaeed40a7d88b1341312efbd27568cf
Henrik Bulskov <bulskov@ruc.dk>
2021-11-23 09:50:21 +0100

    simple html and js example

---
+++ WebService/wwwroot/js/my.js
@@ -0,0 +1,12 @@
+﻿
+
+var btn = document.getElementById("btn");
+var input = document.getElementById("in");
+var output = document.getElementById("out");
+var output2 = document.getElementById("out2");
+
+btn.addEventListener('click', function () {
+    var val = input.value;
+    output.innerHTML = val;
+    output2.innerHTML = val;
+});
