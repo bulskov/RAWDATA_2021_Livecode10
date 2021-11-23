f423c14b9a21447b99ea6e07d257447816f2e826
Henrik Bulskov <bulskov@ruc.dk>
2021-11-23 12:36:00 +0100

    using knockout

---
+++ WebService/wwwroot/js/main.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="lib/jquery/dist/jquery.min.js" />
+/// <reference path="lib/knockout/build/output/knockout-latest.debug.js" />
+
+
+require.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: "lib/jquery/dist/jquery.min",
+        knockout: "lib/knockout/build/output/knockout-latest.debug"
+    }
+});
+
+
+require(["knockout", "viewmodel"], function (ko, vm) {
+    console.log(vm.firstName);
+
+    ko.applyBindings(vm);
+
+});
---
+++ WebService/wwwroot/js/viewmodel.js
@@ -0,0 +1,22 @@
+﻿define(["knockout"], function (ko) {
+
+    let firstName = ko.observable("Peter");
+    let lastName = ko.observable("Smith");
+    let names = ko.observableArray([]);
+
+    let fullName = ko.computed(function () {
+        return firstName() + " " + lastName();
+    });
+
+    let addName = function (data) {
+        names.push({ firstName: data.firstName(), lastName: data.lastName() });
+    }
+
+    return {
+        firstName,
+        lastName,
+        fullName,
+        names,
+        addName
+    }
+});
