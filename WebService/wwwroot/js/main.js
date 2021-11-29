e33546f8d3f510a83ee54dafd26b42922926243a
Henrik Bulskov <bulskov@ruc.dk>
2021-11-29 22:05:07 +0100

    update libman

--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -1,10 +1,12 @@
 ﻿/// <reference path="lib/jquery/dist/jquery.min.js" />
+/// <reference path="lib/requirejs/text.js" />
 /// <reference path="lib/knockout/build/output/knockout-latest.debug.js" />
 
 
 require.config({
     baseUrl: 'js',
     paths: {
+        text: "lib/requirejs/text",
         jquery: "lib/jquery/dist/jquery.min",
         knockout: "lib/knockout/build/output/knockout-latest.debug",
         dataService: "services/dataService"
