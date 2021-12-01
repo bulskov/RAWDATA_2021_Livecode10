da5bc97aff73c66da0a2774b6837ce7dbb0297f0
Henrik Bulskov <bulskov@ruc.dk>
2021-12-02 00:02:27 +0100

    change into livecode 10

--- WebService/wwwroot/js/services/postman.js
+++ WebService/wwwroot/js/services/postman.js
@@ -1,15 +1,12 @@
 ﻿define([], () => {
     let subscribers = [];
-    let lastEvent = undefined;
-
+    
     let subscribe = (event, callback, target) => {
-        let subscriber = { event, callback, target };
+        let subscriber = { event, callback, target};
 
         if (!subscribers.find(x => x.target === target && x.event === event))
             subscribers.push(subscriber);
 
-        if (lastEvent && lastEvent.event === event)
-            callback(lastEvent.data);
     };
 
     let publish = (event, data) => {
@@ -17,8 +14,6 @@
         subscribers.forEach(x => {
             if (x.event === event) x.callback(data);
         });
-
-        lastEvent = { event, data };
     };
 
     return {
