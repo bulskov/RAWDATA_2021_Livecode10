b2a33e557e5347246fd857862da9579a9214b275
Henrik Bulskov <bulskov@ruc.dk>
2021-12-02 11:25:41 +0100

    using a menu

--- WebService/wwwroot/js/main.js
+++ WebService/wwwroot/js/main.js
@@ -1,6 +1,7 @@
 ﻿/// <reference path="lib/jquery/dist/jquery.min.js" />
 /// <reference path="lib/requirejs/text.js" />
 /// <reference path="lib/knockout/build/output/knockout-latest.debug.js" />
+/// <reference path="../css/lib/bootstrap/dist/js/bootstrap.bundle.min.js" />
 
 
 require.config({
@@ -9,6 +10,7 @@ require.config({
         text: "lib/requirejs/text",
         jquery: "lib/jquery/dist/jquery.min",
         knockout: "lib/knockout/build/output/knockout-latest.debug",
+        bootstrap: "../css/lib/bootstrap/dist/js/bootstrap.bundle.min",
         dataService: "services/dataService",
         postman: "services/postman"
     }
@@ -24,9 +26,23 @@ require(['knockout'], (ko) => {
         viewModel: { require: "components/categories/listCategories" },
         template: { require: "text!components/categories/listCategories.html" }
     });
+
+    ko.bindingHandlers.currency = {
+        update: function (element, valueAccessor, allBindings) {
+            // First get the latest data that we're bound to
+            var value = valueAccessor();
+
+            // Next, whether or not the supplied model property is observable, get its current value
+            var valueUnwrapped = ko.unwrap(value);
+
+            element.innerText = "$" + Number(valueUnwrapped).toFixed(2);
+
+ 
+        }
+    };
 });
 
-require(["knockout", "viewmodel"], function (ko, vm) {
+require(["knockout", "viewmodel", "bootstrap"], function (ko, vm) {
     //console.log(vm.firstName);
 
     ko.applyBindings(vm);
--- WebService/wwwroot/js/viewmodel.js
+++ WebService/wwwroot/js/viewmodel.js
@@ -1,12 +1,32 @@
 ﻿define(["knockout", "postman"], function (ko, postman) {
 
-    let currentView = ko.observable("list-categories");
+    let amount = ko.observable(123.4567);
+
+    let menuItems = [
+        { title: "List", component: "list-categories" },
+        { title: "Add", component: "add-category" }
+    ];
+
+    let currentView = ko.observable(menuItems[0].component);
+
+    let changeContent = menuItem => {
+        console.log(menuItem);
+        currentView(menuItem.component)
+    };
+
+    let isActive = menuItem => {
+        return menuItem.component === currentView() ? "active" : "";
+    }
 
     postman.subscribe("changeView", function (data) {
         currentView(data);
     });
     
     return {
-        currentView
+        currentView,
+        menuItems,
+        changeContent,
+        isActive,
+        amount
     }
 });
